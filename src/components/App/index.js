e9f03ad65d06029a5461e23426fd40e318d58ed3
Mykyta Rahmany <nikita.rahmany@gmail.com>
2021-08-13 12:24:28 +0300

    changed paths to homepage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -21,7 +21,7 @@ export default function App() {
           <div className='menu-inner'>
           <ul className='menu-list'>
             <li className='menu-list__item'>
-              <NavLink activeClassName='menu__link--active' className='menu__link' exact to={"/"}>Home</NavLink>
+              <NavLink activeClassName='menu__link--active' className='menu__link' exact to={"/home"}>Home</NavLink>
             </li>
             <li className='menu-list__item'>
               <NavLink activeClassName='menu__link--active' className='menu__link' to={"/posts"}>Posts</NavLink>
@@ -37,7 +37,7 @@ export default function App() {
         </nav>
 
         <Switch>
-        <Route exact path={"/"}>
+        <Route exact path={"/home"}>
           <Homepage/>
         </Route>
           <Route path={"/posts"}>
